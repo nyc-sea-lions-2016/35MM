238c374a4e3bd952611abcd4a288a022251d28a7
Case104 <jpcase104@gmail.com>
2016-04-18 17:42:55 -0400

    Video added

--- thirty-five-mm/config/routes.rb
+++ thirty-five-mm/config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+resources :categories
+
+root 'categories#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
