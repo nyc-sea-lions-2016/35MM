a3f4b783fa9d9a6e54ac491fa06676e1b4559fce
Pietro-Martini <email": null>
2016-04-18 16:06:06 -0400

    added show method to controller

--- thirty-five-mm/app/controllers/films_controller.rb
+++ thirty-five-mm/app/controllers/films_controller.rb
@@ -1,15 +1,19 @@
 class FilmsController < ApplicationController
 
   def index
+
   end
 
   def new
+    @film = Film.new(params[:id])
   end
 
   def create
+    @film = Film.new(film_params)
   end
 
   def show
+    @film = Film.find(params[:id])
   end
 
   def edit
@@ -19,6 +23,12 @@ class FilmsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def film_params
+    params.require(:film).permit(:title)
   end
 
 end
