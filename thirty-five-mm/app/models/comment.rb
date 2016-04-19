294e8166b611d5c4848db9666dcd490ce2d9dbfb
Tim Beck <email": null>
2016-04-19 10:20:12 -0400

    Add polymorphic associations to rating and comment models

--- thirty-five-mm/app/models/comment.rb
+++ thirty-five-mm/app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
-  belongs_to :review
-  belongs_to :film
+  # belongs_to :review
+  # belongs_to :film
+  belongs_to :commentable, polymorphic: true
 
   validates :content, :user, presence: true
 end
