4caa4dec9de056602947c81fd29708fbd9568886
akeem-s <akeemstreet@gmail.com>
2016-04-20 01:07:37 -0400

    Add categories index styling, main feature video

--- thirty-five-mm/app/controllers/categories_controller.rb
+++ thirty-five-mm/app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
 
 	def index
     @categories = Category.all
+    @films = Film.all
 		render :index
 	end
-
 end
--- thirty-five-mm/app/models/user.rb
+++ thirty-five-mm/app/models/user.rb
@@ -6,4 +6,12 @@ class User < ActiveRecord::Base
 
   validates :password, presence: true
   validates :email, :username, presence: true, uniqueness: true
+
+  def review_count
+    self.reviews.count
+  end
+
+  def comment_count
+    self.comments.count
+  end
 end
