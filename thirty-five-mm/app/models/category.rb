d2ac23aff94eda1bb9aae2bd19bfe01651b339f2
Akeem Street <akeemstreet@gmail.com>
2016-05-06 14:19:47 -0400

    Remove unused methods

--- thirty-five-mm/app/models/category.rb
+++ thirty-five-mm/app/models/category.rb
@@ -10,26 +10,8 @@ class Category < ActiveRecord::Base
   def alphabetize
     self.order(:name)
   end
-  # def top_five
-  #   ratings = Rating.where(ratable_type: "Film")
-  #   ratings.order("stars DESC")
-  #   top_five_ratings = []
-  #   top_five_films = []
-
-  #   5.times do
-  #     f = ratings.first
-  #     top_five_ratings.push(f)
-  #   end
-
-  #   top_five_ratings.each do |r|
-  #     f = Film.find(r.ratable_id)
-  #     top_five_films.push(f)
-  #   end
-  #   top_five_films
-  # end
 
   def feature_film
     films = Film.all
-
   end
 end
