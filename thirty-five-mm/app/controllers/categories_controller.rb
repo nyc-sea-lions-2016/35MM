87049118b84181247d1e032df50c1d7616983bc7
Pietro Martini <pmartini.crotti@gmail.com>
2016-05-14 18:48:35 -0400

    limit rendering of films in category index to first fifteen

--- thirty-five-mm/app/controllers/categories_controller.rb
+++ thirty-five-mm/app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
 
 	def index
     @categories = Category.order(:name)
-    @films = Film.order(:title)
+    @films = Film.order(:title).limit(15)
 		render :index
 	end
 end
